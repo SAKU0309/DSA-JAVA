23484e883864e7d2d8e6704d632fe5d19a850f9d
Sakshi <sakshichy14@gmail.com>
2025-02-21 00:11:52 +0530

    merging and moving 0 to last

---
+++ ARRAY/merge 2 sorted array/Solution.java
@@ -0,0 +1,25 @@
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class Solution {
+    public static List< Integer > sortedArray(int []a, int []b){
+        Set<Integer> set = new HashSet<>();
+        for(int num : a) {
+            set.add(num);
+        }
+        for(int num:b){
+            set.add(num);
+        }
+        List<Integer> result = new ArrayList<>(set);
+        Collections.sort(result);
+        return result;
+    }
+    public static void main(String[] args) {
+        int[] a={1,2,3,4,5};
+        int[] b={1,3,4,6,7,8};
+        System.out.println(sortedArray(a, b));
+    }
+}
---
+++ ARRAY/move zero/Solution.java
@@ -0,0 +1,22 @@
+
+import java.util.Arrays;
+
+
+public class Solution {
+    public static void main(String[] args) {
+        int[] arr={0,1,0,3,12};
+        movezero(arr);
+        System.out.println(Arrays.toString(arr));
+    }
+    static void movezero(int[] arr){
+        int index=0;
+        for(int num : arr){
+            if(num!=0){
+                arr[index++]=num;
+            }
+        }
+        while(index<arr.length){
+            arr[index++]=0;
+        }
+    }
+}
