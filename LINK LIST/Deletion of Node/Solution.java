16c7b6da8505b91958c52cef77b64959516be606
Sakshi <sakshichy14@gmail.com>
2025-02-23 21:00:31 +0530

    linked list

--- LINK LIST/Deletion of Node/Solution.java
+++ LINK LIST/Deletion of Node/Solution.java
@@ -1,3 +1,5 @@
+
+
 class Node{
     int data;
     Node next;
@@ -6,7 +8,11 @@ class Node{
         this.next = null;
     }
 }
+
 public class Solution {
+/*
+ *  delete at start
+ */
     static Node deleteHead(Node head){
         if(head==null){
             return null;
@@ -21,13 +27,61 @@ public class Solution {
         }
         System.out.println("NULL");
     }
+    /*
+     * Delete a Linked List node at a given position
+     */
+    static Node deleteatposition(Node head,int position){
+        Node temp=head;
+        Node prev=null;
+        if(temp==null){
+            return head;
+        }
+        if(position==1){
+            head=temp.next;
+            return head;
+        }
+        for (int i = 1; temp != null && i < position; i++){
+            prev = temp;
+            temp=temp.next;
+        }
+        if(temp!=null){
+            prev.next=temp.next;
+        }
+        else{
+            System.out.println("data not found");
+        }
+        return head;  
+    }
+    /*
+     * Deletion at the End of Linked List
+    */
+    static Node deleteatend(Node head){
+        if(head==null){
+            return null;
+        }
+        if(head.next==null){
+            return null;
+        }
+        Node secondlastnode = head;
+        while(secondlastnode.next.next!=null){
+            secondlastnode=secondlastnode.next;
+        }
+        secondlastnode.next=null;
+        return head;
+    }
+
     public static void main(String[] args) {
         Node head = new Node(3);
         head.next = new Node(5);
         head.next.next = new Node(12);
         head.next.next.next = new Node(15);
         head.next.next.next.next = new Node(18);
+        print(head);
         head = deleteHead(head);
+        print(head);
+        head = deleteatposition(head, 3);
+        print(head);
+        head = deleteatend(head);
         print(head);
     }
 }
